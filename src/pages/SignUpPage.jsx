c63ebecff8867911ba9c48df6f4fb8616413a8ad
danondanissimo <106009630+danondanissimo@users.noreply.github.com>
2024-07-18 18:29:05 +0100

    Added link and data dispatch

---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,12 @@
+import SignUpForm from 'components/SignUpForm/SignUpForm';
+import React from 'react';
+
+const SignUpPage = () => {
+  return (
+    <div>
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default SignUpPage;
