58f3cbece9021ef33c41a3dcade1d9486dd9c974
Vitalii-250885 <provitaleks@gmail.com>
2024-07-18 20:49:31 +0300

    add imgs

--- src/components/UserBar/UserBar.jsx
+++ src/components/UserBar/UserBar.jsx
@@ -5,7 +5,7 @@ const UserBar = ({ onClick, isShow }) => {
     <>
       <button type="button" onClick={onClick} className={css.button}>
         <p>Nadia</p>
-        <img src="../../img/Ellipse 14.png" alt="" />
+        <img src="../../../public/Ellipse 14.png" alt="" />
         {isShow ? (
           <img src="../../img/chevron-up.svg" alt="" />
         ) : (
