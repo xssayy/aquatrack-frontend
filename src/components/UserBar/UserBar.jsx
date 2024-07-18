e5762140e14464720038dd98be037d950fb838b2
Vitalii-250885 <provitaleks@gmail.com>
2024-07-18 21:03:09 +0300

    imgs

--- src/components/UserBar/UserBar.jsx
+++ src/components/UserBar/UserBar.jsx
@@ -5,7 +5,7 @@ const UserBar = ({ onClick, isShow }) => {
     <>
       <button type="button" onClick={onClick} className={css.button}>
         <p>Nadia</p>
-        <img src="../../../public/Ellipse 14.png" alt="" />
+        <img src="./Ellipse14.png" alt="" className={css.avatar} />
         {isShow ? (
           <img src="../../img/chevron-up.svg" alt="" />
         ) : (
