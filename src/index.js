3f429fb646c8060203990b2a684d9e76e3fc7180
Mykyta Kudlai <82396742+xssayy@users.noreply.github.com>
2024-07-19 06:08:50 +0200

    try to fix error

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { App } from 'components/App.jsx';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import { store } from 'redux/store.js';
+import store from 'redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
