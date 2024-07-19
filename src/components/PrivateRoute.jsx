cc334918f8d9f0fae6884d8bce4f8f00b05f3712
Mykyta Kudlai <82396742+xssayy@users.noreply.github.com>
2024-07-19 06:28:22 +0200

    deploy err fix

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+
 // import { selectIsLoggedIn } from '../redux/auth/selectors';
 
 export const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
