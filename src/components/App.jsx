66d031c58ea83e140d2ae2c5b9515045962df51c
Mykyta Kudlai <82396742+xssayy@users.noreply.github.com>
2024-07-20 01:06:02 +0200

    upd icon component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,32 +10,31 @@ import Icon from './shared/Icon';
 
 export const App = () => {
   return (
-    <Icon id="plus" width={32} height={32} />
-    // <Layout>
-    //   <Routes>
-    //     <Route path="/" element={<WelcomePage />} />
-    //     <Route
-    //       path="/signup"
-    //       element={
-    //         <RestrictedRoute
-    //           redirectTo="/tracker"
-    //           component={<RegistrationPage />}
-    //         />
-    //       }
-    //     />
-    //     <Route
-    //       path="/signin"
-    //       element={
-    //         <RestrictedRoute redirectTo="/tracker" component={<LoginForm />} />
-    //       }
-    //     />
-    //     <Route
-    //       path="/tracker"
-    //       element={
-    //         <PrivateRoute redirectTo="/signin" component={<HomePage />} />
-    //       }
-    //     />
-    //   </Routes>
-    // </Layout>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route
+          path="/signup"
+          element={
+            <RestrictedRoute
+              redirectTo="/tracker"
+              component={<RegistrationPage />}
+            />
+          }
+        />
+        <Route
+          path="/signin"
+          element={
+            <RestrictedRoute redirectTo="/tracker" component={<LoginForm />} />
+          }
+        />
+        <Route
+          path="/tracker"
+          element={
+            <PrivateRoute redirectTo="/signin" component={<HomePage />} />
+          }
+        />
+      </Routes>
+    </Layout>
   );
 };
