39587091ae3c6ced3c8dc0fe204bcab2a3edae74
NikOvchinnik <n.ovchinnik.s@gmail.com>
2024-07-21 00:46:49 +0300

    scroll

--- src/components/UserSettingsForm/UserSettingsForm.jsx
+++ src/components/UserSettingsForm/UserSettingsForm.jsx
@@ -4,6 +4,8 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
 import style from './UserSettingsForm.module.css';
 import Icon from 'components/shared/Icon';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import 'react-perfect-scrollbar/dist/css/styles.css';
 
 const initialState = {
   photo: '',
@@ -60,7 +62,7 @@ const UserSettingsForm = () => {
 
   return (
     <form className={style.formContainer} onSubmit={handleSubmit(onSubmit)}>
-      <div>
+      <div className={style.upperContainer}>
         <div className={style.imgContainer}>
           <img
             className={style.userImg}
@@ -79,148 +81,150 @@ const UserSettingsForm = () => {
         />
         {errors.photo && <p>{errors.photo.message}</p>}
       </div>
-      <div className={style.settingContainer}>
-        <div>
-          <h3 className={style.radioTitle}>Your gender identity</h3>
-          <div className={style.radioContainer}>
-            <div className={style.inputRadioContainer}>
-              <input
-                className={style.inputRadio}
-                id={idGenderWoman}
-                type="radio"
-                {...register('gender')}
-                value="woman"
-              />
-              <label className={style.labelRadio} htmlFor={idGenderWoman}>
-                Woman
+      <PerfectScrollbar className={style.scrollContainer}>
+        <div className={style.settingContainer}>
+          <div>
+            <h3 className={style.radioTitle}>Your gender identity</h3>
+            <div className={style.radioContainer}>
+              <div className={style.inputRadioContainer}>
+                <input
+                  className={style.inputRadio}
+                  id={idGenderWoman}
+                  type="radio"
+                  {...register('gender')}
+                  value="woman"
+                />
+                <label className={style.labelRadio} htmlFor={idGenderWoman}>
+                  Woman
+                </label>
+              </div>
+              <div className={style.inputRadioContainer}>
+                <input
+                  className={style.inputRadio}
+                  id={idGenderMan}
+                  type="radio"
+                  {...register('gender')}
+                  value="man"
+                />
+                <label className={style.labelRadio} htmlFor={idGenderMan}>
+                  Man
+                </label>
+              </div>
+              {errors.gender && <p>{errors.gender.message}</p>}
+            </div>
+          </div>
+
+          <div className={style.userInfoContainer}>
+            <div className={style.inputContainer}>
+              <label className={style.labelTitle} htmlFor={idName}>
+                Your name
               </label>
-            </div>
-            <div className={style.inputRadioContainer}>
               <input
-                className={style.inputRadio}
-                id={idGenderMan}
-                type="radio"
-                {...register('gender')}
-                value="man"
+                className={style.inputElem}
+                type="text"
+                id={idName}
+                {...register('name')}
               />
-              <label className={style.labelRadio} htmlFor={idGenderMan}>
-                Man
+              {errors.name && <p>{errors.name.message}</p>}
+            </div>
+
+            <div className={style.inputContainer}>
+              <label className={style.labelTitle} htmlFor={idEmail}>
+                Email
               </label>
-            </div>
-            {errors.gender && <p>{errors.gender.message}</p>}
-          </div>
-        </div>
-
-        <div className={style.userInfoContainer}>
-          <div className={style.inputContainer}>
-            <label className={style.labelTitle} htmlFor={idName}>
-              Your name
-            </label>
-            <input
-              className={style.inputElem}
-              type="text"
-              id={idName}
-              {...register('name')}
-            />
-            {errors.name && <p>{errors.name.message}</p>}
-          </div>
-
-          <div className={style.inputContainer}>
-            <label className={style.labelTitle} htmlFor={idEmail}>
-              Email
-            </label>
-            <input
-              className={style.inputElem}
-              id={idEmail}
-              type="email"
-              {...register('email')}
-            />
-            {errors.email && <p>{errors.email.message}</p>}
-          </div>
-        </div>
-
-        <div className={style.dailyNormaContainer}>
-          <h3 className={style.labelTitle}>My daily norma</h3>
-          <div className={style.dailyNormaFormulaContainer}>
-            <div>
-              <p className={style.dailyNormaGender}>For woman:</p>
-              <p className={style.dailyNormaFormula}>V=(M*0,03) + (T*0,4)</p>
-            </div>
-            <div>
-              <p className={style.dailyNormaGender}>For man:</p>
-              <p className={style.dailyNormaFormula}>V=(M*0,04) + (T*0,6)</p>
+              <input
+                className={style.inputElem}
+                id={idEmail}
+                type="email"
+                {...register('email')}
+              />
+              {errors.email && <p>{errors.email.message}</p>}
             </div>
           </div>
-          <div className={style.dailyNormaDescriptionContainer}>
-            <p className={style.dailyNormaDescription}>
-              <span className={style.dailyNormaDescriptionSpan}>*</span> V is
-              the volume of the water norm in liters per day, M is your body
-              weight, T is the time of active sports, or another type of
-              activity commensurate in terms of loads (in the absence of these,
-              you must set 0)
-            </p>
-          </div>
-          <p className={style.formaText}>
-            <span className={style.dailyNormaWarningSpan}>!</span> Active time
-            in hours
-          </p>
-        </div>
 
-        <div className={style.userInfoContainer}>
-          <div className={style.inputContainer}>
-            <label className={style.formaText} htmlFor={idWeight}>
-              Your weight in kilograms:
-            </label>
-            <input
-              className={style.inputElem}
-              id={idWeight}
-              type="number"
-              {...register('weight')}
-            />
-            {errors.weight && <p>{errors.weight.message}</p>}
-          </div>
-
-          <div className={style.inputContainer}>
-            <label className={style.formaText} htmlFor={idSportHours}>
-              The time of active participation in sports:
-            </label>
-            <input
-              className={style.inputElem}
-              id={idSportHours}
-              type="number"
-              {...register('sportHours')}
-            />
-            {errors.sportHours && <p>{errors.sportHours.message}</p>}
-          </div>
-        </div>
-
-        <div className={style.userInfoContainer}>
-          <div className={style.inputContainer}>
+          <div className={style.dailyNormaContainer}>
+            <h3 className={style.labelTitle}>My daily norma</h3>
+            <div className={style.dailyNormaFormulaContainer}>
+              <div>
+                <p className={style.dailyNormaGender}>For woman:</p>
+                <p className={style.dailyNormaFormula}>V=(M*0,03) + (T*0,4)</p>
+              </div>
+              <div>
+                <p className={style.dailyNormaGender}>For man:</p>
+                <p className={style.dailyNormaFormula}>V=(M*0,04) + (T*0,6)</p>
+              </div>
+            </div>
+            <div className={style.dailyNormaDescriptionContainer}>
+              <p className={style.dailyNormaDescription}>
+                <span className={style.dailyNormaDescriptionSpan}>*</span> V is
+                the volume of the water norm in liters per day, M is your body
+                weight, T is the time of active sports, or another type of
+                activity commensurate in terms of loads (in the absence of
+                these, you must set 0)
+              </p>
+            </div>
             <p className={style.formaText}>
-              The required amount of water in liters per day:
+              <span className={style.dailyNormaWarningSpan}>!</span> Active time
+              in hours
             </p>
-            <p className={style.dailyNormaFormula}>1.8 L</p>
           </div>
 
-          <div className={style.inputContainer}>
-            <label className={style.labelTitle} htmlFor={idWaterAmount}>
-              Write down how much water you will drink:
-            </label>
-            <input
-              className={style.inputElem}
-              type="number"
-              step="0.1"
-              id={idWaterAmount}
-              {...register('waterAmount')}
-            />
-            {errors.waterAmount && <p>{errors.waterAmount.message}</p>}
+          <div className={style.userInfoContainer}>
+            <div className={style.inputContainer}>
+              <label className={style.formaText} htmlFor={idWeight}>
+                Your weight in kilograms:
+              </label>
+              <input
+                className={style.inputElem}
+                id={idWeight}
+                type="number"
+                {...register('weight')}
+              />
+              {errors.weight && <p>{errors.weight.message}</p>}
+            </div>
+
+            <div className={style.inputContainer}>
+              <label className={style.formaText} htmlFor={idSportHours}>
+                The time of active participation in sports:
+              </label>
+              <input
+                className={style.inputElem}
+                id={idSportHours}
+                type="number"
+                {...register('sportHours')}
+              />
+              {errors.sportHours && <p>{errors.sportHours.message}</p>}
+            </div>
           </div>
+
+          <div className={style.userInfoContainer}>
+            <div className={style.inputContainer}>
+              <p className={style.formaText}>
+                The required amount of water in liters per day:
+              </p>
+              <p className={style.dailyNormaFormula}>1.8 L</p>
+            </div>
+
+            <div className={style.inputContainer}>
+              <label className={style.labelTitle} htmlFor={idWaterAmount}>
+                Write down how much water you will drink:
+              </label>
+              <input
+                className={style.inputElem}
+                type="number"
+                step="0.1"
+                id={idWaterAmount}
+                {...register('waterAmount')}
+              />
+              {errors.waterAmount && <p>{errors.waterAmount.message}</p>}
+            </div>
+          </div>
+
+          <button className={style.formButton} type="submit">
+            Save
+          </button>
         </div>
-
-        <button className={style.formButton} type="submit">
-          Save
-        </button>
-      </div>
+      </PerfectScrollbar>
     </form>
   );
 };
