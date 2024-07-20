3cff70eb7ec9a7e7add6c7adde77c98fb1cc78d0
Vitalii-250885 <provitaleks@gmail.com>
2024-07-20 18:22:27 +0300

    connect buttons with modal window

--- src/components/UserBarPopover/UserBarPopover.jsx
+++ src/components/UserBarPopover/UserBarPopover.jsx
@@ -1,14 +1,40 @@
+import { useState } from 'react';
 import css from './UserBarPopover.module.css';
+import ModalWindow from 'components/ModalWindow/ModalWindow';
 
 const UserBarPopover = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function openModal() {
+    setModalIsOpen(true);
+  }
+
+  function closeModal() {
+    setModalIsOpen(false);
+  }
+
   return (
     <div className={css['user-bar-popover-container']}>
-      <button type="button" className={css.settings}>
-        <img src="../../img/settings.svg" alt="" /> Setting
+      <button type="button" onClick={openModal} className={css.settings}>
+        <svg className={css['icon-setting']}>
+          <use href="sprite.svg#icon-settings"></use>
+        </svg>
+        Setting
       </button>
-      <button type="button" className={css['log-out']}>
-        <img src="../../img/log-out.svg" alt="" /> Log out
+      <ModalWindow
+        modalIsOpen={modalIsOpen}
+        onCloseModal={closeModal}
+      ></ModalWindow>
+      <button type="button" onClick={openModal} className={css['log-out']}>
+        <svg className={css['icon-log-out']}>
+          <use href="sprite.svg#icon-log-out"></use>
+        </svg>
+        Log out
       </button>
+      <ModalWindow
+        modalIsOpen={modalIsOpen}
+        onCloseModal={closeModal}
+      ></ModalWindow>
     </div>
   );
 };
