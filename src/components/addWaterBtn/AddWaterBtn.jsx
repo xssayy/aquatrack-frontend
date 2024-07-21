4be500acf32035e49a2f313c4e9af00c23491784
Sakrid1s <bilinets.vitalik@gmail.com>
2024-07-21 19:00:23 +0300

    upd

--- src/components/addWaterBtn/AddWaterBtn.jsx
+++ src/components/addWaterBtn/AddWaterBtn.jsx
@@ -8,6 +8,7 @@ import css from './AddWaterBtn.module.css';
 
 const AddWaterBtn = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [iconFillColor, setIconFillColor] = useState('#ffffff');
 
   const handleAddWater = () => {
     setIsOpen(true);
@@ -17,19 +18,29 @@ const AddWaterBtn = () => {
     setIsOpen(false);
   };
 
+  const handleMouseEnter = () => {
+    setIconFillColor('#9be1a0');
+  };
+
+  const handleMouseLeave = () => {
+    setIconFillColor('#ffffff');
+  };
+
   return (
     <div className={css.addWaterBtnContainer}>
       <button
         className={css.addWaterBtn}
         type="button"
         onClick={handleAddWater}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
       >
         <Icon
           id={'plus'}
           width={16}
           height={16}
           className={css.iconPlus}
-          fillColor={'#fff'}
+          fillColor={iconFillColor}
         />
         Add water
       </button>
