b965db96714d8f46f3b4d437a6cfb8aaad9ad45b
NikOvchinnik <n.ovchinnik.s@gmail.com>
2024-07-22 01:04:36 +0300

    delete welcome page info

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,28 +1,5 @@
-import ModalWindow from "components/ModalWindow/ModalWindow";
-import UserSettingsModal from "components/UserSettingsModal/UserSettingsModal";
-import { useState } from 'react';
-
 const WelcomePage = () => {
-    const [modalIsOpen, setModalIsOpen] = useState(false);
-
-    function openModal() {
-      setModalIsOpen(true);
-  }
-
-    function closeModal() {
-      setModalIsOpen(false);
-    }
-
-  return (
-    <>
-      <button type="button" onClick={openModal}>
-        Open Modal
-      </button>
-      <ModalWindow modalIsOpen={modalIsOpen} onCloseModal={closeModal}>
-        <UserSettingsModal onCloseModal={closeModal} />
-      </ModalWindow>
-    </>
-  );
+  return <></>;
 };
 
 export default WelcomePage;
