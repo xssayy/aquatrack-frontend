21f90876cafb48ffe2e2e00453a00bb848fa87d2
Sakrid1s <bilinets.vitalik@gmail.com>
2024-07-21 18:33:55 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,44 @@
-import { Route, Routes } from 'react-router-dom';
-import { Layout } from './Layout';
-import { RestrictedRoute } from './RestrictedRoute';
-import { PrivateRoute } from './PrivateRoute';
-import WelcomePage from 'pages/WelcomePage';
-import RegistrationPage from 'pages/RegistrationPage';
-import LoginForm from 'pages/LoginForm';
+// import { Route, Routes } from 'react-router-dom';
+// import { Layout } from './Layout';
+// import { RestrictedRoute } from './RestrictedRoute';
+// import { PrivateRoute } from './PrivateRoute';
+// import WelcomePage from 'pages/WelcomePage';
+// import RegistrationPage from 'pages/RegistrationPage';
+// import LoginForm from 'pages/LoginForm';
+// import HomePage from 'pages/HomePage';
 import HomePage from 'pages/HomePage';
+// import WaterMainInfo from './waterMainInfo/WaterMainInfo';
 
 export const App = () => {
   return (
-    <Layout>
-      <Routes>
-        <Route path="/" element={<WelcomePage />} />
-        <Route
-          path="/signup"
-          element={
-            <RestrictedRoute
-              redirectTo="/tracker"
-              component={<RegistrationPage />}
-            />
-          }
-        />
-        <Route
-          path="/signin"
-          element={
-            <RestrictedRoute redirectTo="/tracker" component={<LoginForm />} />
-          }
-        />
-        <Route
-          path="/tracker"
-          element={
-            <PrivateRoute redirectTo="/signin" component={<HomePage />} />
-          }
-        />
-      </Routes>
-    </Layout>
+    <div>
+      <HomePage />
+    </div>
+    // <Layout>
+    //   <Routes>
+    //     <Route path="/" element={<WelcomePage />} />
+    //     <Route
+    //       path="/signup"
+    //       element={
+    //         <RestrictedRoute
+    //           redirectTo="/tracker"
+    //           component={<RegistrationPage />}
+    //         />
+    //       }
+    //     />
+    //     <Route
+    //       path="/signin"
+    //       element={
+    //         <RestrictedRoute redirectTo="/tracker" component={<LoginForm />} />
+    //       }
+    //     />
+    //     <Route
+    //       path="/tracker"
+    //       element={
+    //         <PrivateRoute redirectTo="/signin" component={<HomePage />} />
+    //       }
+    //     />
+    //   </Routes>
+    // </Layout>
   );
 };
