7774e635b0254a22e7ed06b078c24046c854231e
SerhiiBasok <sergeybsk19@gmail.com>
2024-07-23 17:35:32 +0300

    DeleteWaterModal and css

---
+++ src/components/DeleteWaterModal/DeleteWaterModal.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import ModalWindow from '../ModalWindow/ModalWindow';
+// import { toast } from 'react-toastify';
+// import { useDispatch } from 'react-redux';
+// import axios from 'axios';
+import styles from './DeleteWaterModal.module.css';
+
+const DeleteWaterModal = ({ isOpen, closeModal, recordId }) => {
+  const [isProcessing, setIsProcessing] = useState(false);
+  //   const dispatch = useDispatch();
+
+  //   const handleDelete = async () => {
+  //     setIsProcessing(true);
+  //     try {
+  //       await axios.delete(`/api/water/${recordId}`);
+  //       toast.success('Record deleted successfully');
+  //       dispatch(); // Оновлення даних за допомогою Redux
+  //     } catch (error) {
+  //       toast.error('Failed to delete record');
+  //     } finally {
+  //       closeModal();
+  //       setIsProcessing(false);
+  //     }
+  //   };
+
+  return (
+    <ModalWindow modalIsOpen={isOpen} onCloseModal={closeModal}>
+      <h2 className={styles.title}>Delete entry</h2>
+      <p className={styles.question}>
+        Are you sure you want to delete the entry?
+      </p>
+      <div className={styles.buttonContainer}>
+        <button
+          type="button"
+          //   onClick={handleDelete}
+          className={`${styles.commonBtn} ${styles.deleteBtn}`}
+        >
+          Delete
+        </button>
+        <button
+          type="button"
+          onClick={closeModal}
+          className={`${styles.commonBtn} ${styles.cancelBtn}`}
+          disabled={isProcessing}
+        >
+          Cancel
+        </button>
+      </div>
+    </ModalWindow>
+  );
+};
+
+export default DeleteWaterModal;
