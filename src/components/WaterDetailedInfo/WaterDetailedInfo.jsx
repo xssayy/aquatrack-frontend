907d734f6e239fab27fd8c1bccca91ccd5ad3d48
Vitalii-250885 <provitaleks@gmail.com>
2024-07-23 10:19:40 +0300

    Update WaterDetailedInfo.jsx

--- src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
+++ src/components/WaterDetailedInfo/WaterDetailedInfo.jsx
@@ -1,15 +1,19 @@
+import { useState } from 'react';
+
 import UserPanel from '../UserPanel/UserPanel.jsx';
 import DailyInfo from '../DailyInfo/DailyInfo.jsx';
-// import { MonthInfo } from '../MonthInfo/MonthInfo.jsx';
+import { MonthInfo } from '../MonthInfo/MonthInfo.jsx';
 
 import css from './WaterDetailedInfo.module.css';
 
 const WaterDetailedInfo = () => {
+  const [chosenDate, setChosenDate] = useState(new Date().toISOString());
+
   return (
     <div className={css['water-detailed-info-container']}>
       <UserPanel />
-      <DailyInfo />
-      {/* <MonthInfo /> */}
+      <DailyInfo value={chosenDate} setChosenDate={setChosenDate} />
+      <MonthInfo value={chosenDate} setChosenDate={setChosenDate} />
     </div>
   );
 };
