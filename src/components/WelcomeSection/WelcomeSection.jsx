47270dc2d2b37df6a12ffa7aeca4a2daae7fbda4
Mykyta Kudlai <82396742+xssayy@users.noreply.github.com>
2024-07-24 11:09:01 +0200

    fix router

--- src/components/WelcomeSection/WelcomeSection.jsx
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -12,10 +12,10 @@ export const WelcomeSection = () => {
       <p className={css.welcomeText}>Record daily water intake and track</p>
       <h1 className={css.welcomeTitle}>Water consumption tracker</h1>
       <div className={css.welcomeThumb}>
-        <NavLink className={css.linkTracker} to="/aquatrack-frontend/signup">
+        <NavLink className={css.linkTracker} to="/signup">
           Try tracker
         </NavLink>
-        <NavLink className={css.linkSignIn} to="/aquatrack-frontend/signin">
+        <NavLink className={css.linkSignIn} to="/signin">
           Sign in
         </NavLink>
       </div>
