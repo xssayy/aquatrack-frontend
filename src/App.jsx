5522cc302bac66e8b807925660535dc6e005496d
Mykyta Kudlai <82396742+xssayy@users.noreply.github.com>
2024-07-26 14:20:05 +0200

    project transfer finished

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+// import { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { refreshUser } from '../redux/auth/operations';
+// import { selectIsLoggedIn } from '../redux/auth/selectors';
+
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import { RestrictedRoute } from './components/RestrictedRoute';
+import { PrivateRoute } from './components/PrivateRoute';
+import WelcomePage from './pages/WelcomePage/WelcomePage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import HomePage from './pages/HomePage/HomePage';
+import 'modern-normalize';
+import SignUpPage from './pages/SignUpPage/SignUpPage';
+import SingInPage from './pages/SignInPage/SignInPage';
+
+export const App = () => {
+  // const dispatch = useDispatch();
+
+  // const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  // useEffect(() => {
+  //   if (isLoggedIn) {
+  //     dispatch(refreshUser());
+  //   }
+  // }, [dispatch, isLoggedIn]);
+
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+        <Route
+          path="/signup"
+          element={
+            <RestrictedRoute redirectTo="/tracker" component={<SignUpPage />} />
+          }
+        />
+        <Route
+          path="/signin"
+          element={
+            <RestrictedRoute redirectTo="/tracker" component={<SingInPage />} />
+          }
+        />
+        <Route
+          path="/tracker"
+          element={
+            <PrivateRoute redirectTo="/signin" component={<HomePage />} />
+          }
+        />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Layout>
+  );
+};
