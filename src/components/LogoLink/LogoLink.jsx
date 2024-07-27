677dc36bb92696228255e53df5780983af58bac6
Mykyta Kudlai <82396742+xssayy@users.noreply.github.com>
2024-07-27 23:47:37 +0200

    upd logo link

--- src/components/LogoLink/LogoLink.jsx
+++ src/components/LogoLink/LogoLink.jsx
@@ -2,7 +2,7 @@ import styles from './LogoLink.module.css';
 
 const LogoLink = () => {
   return (
-    <a href="/aquatrack-frontend/" className={styles.title}>
+    <a href="/aquatrack-frontend" className={styles.title}>
       Aquatrack
     </a>
   );
