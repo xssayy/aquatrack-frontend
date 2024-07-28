61376219d0b9894da2b3012ab8fd52e066811086
Mykyta Kudlai <82396742+xssayy@users.noreply.github.com>
2024-07-28 14:08:04 +0200

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store} persistor={persistor}>
       <BrowserRouter>
-        {/* <BrowserRouter basename="/aquatrack-frontend"> */}
         <HelmetProvider>
           <App />
         </HelmetProvider>
