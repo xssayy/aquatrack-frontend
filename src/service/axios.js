d7e63b7e14e535b81afa64c38f40179a45bb2501
NikOvchinnik <n.ovchinnik.s@gmail.com>
2024-07-28 23:03:56 +0300

    form submit

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  // baseURL: 'https://aqua-track-backend.onrender.com',
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://aqua-track-backend.onrender.com',
+  // baseURL: 'http://localhost:3000',
   withCredentials: true,
 });
 
