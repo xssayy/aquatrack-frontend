b98d2919d62d44eac73125e509699722f5536d77
Mykyta Kudlai <82396742+xssayy@users.noreply.github.com>
2024-07-29 12:21:59 +0200

    upd refresh && persist

--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,18 @@ import { persistor, store } from './redux/store.js';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import './App.css';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store} persistor={persistor}>
-      <BrowserRouter>
-        <HelmetProvider>
-          <App />
-        </HelmetProvider>
-      </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <HelmetProvider>
+            <App />
+          </HelmetProvider>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
