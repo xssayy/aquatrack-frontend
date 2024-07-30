fad3eddda9fc2bc3f3dd1c57cc285edd9d1fcbb3
Sakrid1s <bilinets.vitalik@gmail.com>
2024-07-30 18:52:39 +0300

    upd

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ import Loader from './components/Loader/Loader.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      <PersistGate loading={<Loader />} persistor={persistor}>
         <I18nextProvider i18n={i18n}>
           <BrowserRouter>
             <HelmetProvider>
