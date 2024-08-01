a7609a3a3cdd886611b865ee1d41707376ee34e3
xssayy <82396742+xssayy@users.noreply.github.com>
2024-08-01 14:59:17 +0000

    Deploying to gh-pages from @ xssayy/aquatrack-frontend@79129624b310aad907164c0d5440b27fd2c2c33e 🚀

---
+++ assets/DocTitle-BimcoRJ1.js
@@ -0,0 +1,2 @@
+import{j as t,H as s}from"./index-BzvgtuYC.js";const e="_container_16wny_1",o={container:e},c=({children:n})=>t.jsx("div",{className:o.container,children:n});function i({children:n}){return t.jsx(s,{children:t.jsx("title",{children:n})})}export{c as C,i as D};
+//# sourceMappingURL=DocTitle-BimcoRJ1.js.map
--- assets/LogoLink-B3pR2_Ut.js
+++
@@ -1,2 +0,0 @@
-import{j as t,t as s}from"./index-DbE13lpG.js";const o="_title_3o5ur_1",e={title:o},r=()=>t.jsx(s,{to:"/",className:e.title,children:"Aquatrack"});export{r as L};
-//# sourceMappingURL=LogoLink-B3pR2_Ut.js.map
