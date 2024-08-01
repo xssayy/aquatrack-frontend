873d35cf29bcb9a0924da61e5df341d14bf54807
xssayy <82396742+xssayy@users.noreply.github.com>
2024-08-01 13:52:00 +0000

    Deploying to gh-pages from @ xssayy/aquatrack-frontend@06123ef5b4187859157981f048f40a39daed9b06 🚀

--- assets/LogoLink-CdL61-7i.js
+++
@@ -1,2 +0,0 @@
-import{j as t,t as s}from"./index-CQBpLktP.js";const o="_title_3o5ur_1",e={title:o},r=()=>t.jsx(s,{to:"/",className:e.title,children:"Aquatrack"});export{r as L};
-//# sourceMappingURL=LogoLink-CdL61-7i.js.map
