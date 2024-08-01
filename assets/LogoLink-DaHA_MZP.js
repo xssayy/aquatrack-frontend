039f25f817997d11173a39e17f0aac4d3f09a833
xssayy <82396742+xssayy@users.noreply.github.com>
2024-08-01 08:51:49 +0000

    Deploying to gh-pages from @ xssayy/aquatrack-frontend@89918cbf0481c237f67663c76ae3cdad42c8f37d 🚀

---
+++ assets/LogoLink-DaHA_MZP.js
@@ -0,0 +1,2 @@
+import{j as t,t as s}from"./index-B8xa_euy.js";const o="_title_3o5ur_1",e={title:o},r=()=>t.jsx(s,{to:"/",className:e.title,children:"Aquatrack"});export{r as L};
+//# sourceMappingURL=LogoLink-DaHA_MZP.js.map
