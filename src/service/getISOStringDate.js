7fd1f28e8ab8867ec5a5d56dbea8a1e2e8b812fc
Artem <atremat@gmail.com>
2024-08-04 00:09:04 +0300

    time zone bug fix

--- src/service/getISOStringDate.js
+++ src/service/getISOStringDate.js
@@ -2,6 +2,5 @@ import { formatISO } from 'date-fns';
 
 export const getISOStringDate = (date = new Date()) => {
   const isoString = formatISO(date, { representation: 'complete' });
-  console.log(isoString);
   return isoString;
 };
